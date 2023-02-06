64751bcfc76ca08965af5eead82471434b12d0d5
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2023-02-06 11:18:33 +0500

    fix: change shortcut

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
       local mark = require("harpoon.mark")
       local ui = require("harpoon.ui")
 
-      vim.keymap.set("n", "<leader>a", mark.add_file)
+      vim.keymap.set("n", "<leader>ha", mark.add_file)
       vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
       vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end)
