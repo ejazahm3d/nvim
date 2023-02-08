f81b253474d62269a3c101b4f0626c929fa2c292
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2023-02-08 18:51:59 +0500

    feat: theme update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,11 +2,14 @@ return {
   -- add gruvbox
   { "olimorris/onedarkpro.nvim" },
   { "rebelot/kanagawa.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { 'olivercederborg/poimandres.nvim' },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "oxocarbon",
     },
   },
 }
