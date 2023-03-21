d77951f33ea8d84127344e994d244bf7b2e4f604
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2023-03-21 15:07:51 +0500

    feat: add ts playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,4 +5,14 @@ return {
       require("nvim-ts-autotag").setup()
     end,
   },
+  {
+    'nvim-treesitter/playground',
+    keys = {
+      {
+        "<leader>tp",
+        "<cmd>TSPlaygroundToggle<cr>",
+        desc = "Toggle treesitter playground"
+      }
+    }
+  }
 }
