ca475ddd47ac755fcbbd63f9d33b5731e951113f
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2023-05-06 12:26:25 +0500

    feat: update folding options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,14 +3,24 @@
 -- Add any additional options here
 --
 --
-if vim.fn.has "nvim-0.9" == 1 then
-  vim.opt.diffopt:append "linematch:60" -- enable linematch diff algorithm
+if vim.fn.has("nvim-0.9") == 1 then
+  vim.opt.diffopt:append("linematch:60") -- enable linematch diff algorithm
 end
 
-vim.opt.foldenable = true                                      -- enable fold for nvim-ufo
-vim.opt.foldlevel = 99                                         -- set high foldlevel for nvim-ufo
-vim.opt.foldlevelstart = 99                                    -- start with all code unfolded
-vim.opt.foldcolumn = vim.fn.has "nvim-0.9" == 1 and "1" or nil -- show foldcolumn in nvim 0.9
+vim.opt.exrc = true
 
+vim.opt.foldenable = true -- enable fold for nvim-ufo
+vim.opt.foldlevel = 99 -- set high foldlevel for nvim-ufo
+vim.opt.foldlevelstart = 99 -- start with all code unfolded
+vim.opt.foldcolumn = vim.fn.has("nvim-0.9") == 1 and "1" or nil -- show foldcolumn in nvim 0.9
+
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 -- vim.opt.cursorlineopt = "number"
+
+-- Never request typescript-language-server for formatting
+vim.lsp.buf.format({
+  filter = function(client)
+    return client.name ~= "tsserver"
+  end,
+})
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -59,7 +59,23 @@ return {
   },
   {
     "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async" },
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
     event = "VeryLazy",
     opts = {
       fold_virt_text_handler = handler,
@@ -93,6 +109,21 @@ return {
           end
       end,
     },
+    config = function(_, opts)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+      local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+        require("lspconfig")[ls].setup({
+          capabilities = capabilities,
+          -- you can add other fields for setting up lsp server in this table
+        })
+      end
+      require("ufo").setup(opts)
+    end,
     keys = {
       {
         "zR",
