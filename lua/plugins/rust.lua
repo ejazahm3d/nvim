8b8eaa788c442bc966650ffcc32c6b1cb4a4100d
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2023-06-25 16:50:48 +0500

    feat: update rust tools

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -15,30 +15,35 @@ return {
       vim.list_extend(opts.ensure_installed, { "codelldb", "rust-analyzer", "taplo" })
     end,
   },
-
+  {
+    "saecki/crates.nvim",
+    tag = "v0.3.0",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup({
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+      })
+    end,
+  },
   -- correctly setup lspconfig for Rust 🚀
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "simrat39/rust-tools.nvim" },
+    dependencies = { "simrat39/rust-tools.nvim", "saecki/crates.nvim" },
     opts = {
       -- make sure mason installs the server
       setup = {
         rust_analyzer = function(_, opts)
-          require("lazyvim.util").on_attach(function(client, buffer)
-            -- stylua: ignore
-            if client.name == "rust_analyzer" then
-              vim.keymap.set("n", "K", "<cmd>RustHoverActions<cr>", { buffer = buffer, desc = "Hover Actions (Rust)" })
-              vim.keymap.set("n", "<leader>cR", "<cmd>RustCodeAction<cr>",
-                { buffer = buffer, desc = "Code Action (Rust)" })
-            end
-          end)
           local mason_registry = require("mason-registry")
           -- rust tools configuration for debugging support
           local codelldb = mason_registry.get_package("codelldb")
           local extension_path = codelldb:get_install_path() .. "/extension/"
           local codelldb_path = extension_path .. "adapter/codelldb"
           local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-              or extension_path .. "lldb/lib/liblldb.so"
+            or extension_path .. "lldb/lib/liblldb.so"
+
           local rust_tools_opts = vim.tbl_deep_extend("force", opts, {
             dap = {
               adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
