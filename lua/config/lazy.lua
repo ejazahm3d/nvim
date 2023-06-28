43d724800d12aa95803dcd1398468ebc20f0e255
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2023-06-28 20:50:44 +0500

    feat: add vscode extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.vscode" },
     -- { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
   },
